290a08d7aab33dc25e7fe142da9756165aa1b143
sangle1996 <42021108+sangle1996@users.noreply.github.com>
2022-03-16 14:51:19 +0700

    Update V2

--- android/src/main/java/com/nano/flutter/nano_healthkit_plugin/NanoHealthkitPlugin.java
+++ android/src/main/java/com/nano/flutter/nano_healthkit_plugin/NanoHealthkitPlugin.java
@@ -8,14 +8,35 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** NanoHealthkitPlugin */
 public class NanoHealthkitPlugin implements MethodCallHandler {
+    private Context context;
+    private MethodChannel methodChannel;
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "nano_healthkit_plugin");
-    channel.setMethodCallHandler(new NanoHealthkitPlugin());
+//     final MethodChannel channel = new MethodChannel(registrar.messenger(), "nano_healthkit_plugin");
+//     channel.setMethodCallHandler(new NanoHealthkitPlugin());
+    new NanoHealthkitPlugin().onAttachedToEngine(registrar.context(), registrar.messenger());
   }
+  
+  @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
+    }
+
+    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
+        this.context = applicationContext;
+        methodChannel = new MethodChannel(messenger, "nano_healthkit_plugin");
+        methodChannel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        context = null;
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null;
+    }
 
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
+  public void onMethodCall(@NonNull MethodCall call,@NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     } else {
